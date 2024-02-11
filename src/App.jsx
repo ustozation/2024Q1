14656ae95797651d74f3abe4c6bff0692b118bf4
Ali Odiljonov <jahcoding@gmail.com>
2024-02-11 22:02:48 +0500

    feat: useRef lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { todosList } from './utils/mock'
 import './App.css'
@@ -6,19 +6,40 @@ import './App.css'
 function App() {
 	const [todos, setTodos] = useState(todosList)
 	const [newTodo, setNewTodo] = useState('')
+	const ulRef = useRef()
+
+	const handleScroll = () => {
+		console.log('scroll')
+	}
 
 	const addTodo = () => {
 		if (newTodo) {
-			setTodos([{ id: uuidv4(), title: newTodo, completed: false }, ...todos])
+			setTodos(prevTodos => {
+				const newTodoData = {
+					id: uuidv4(),
+					title: newTodo,
+					completed: false,
+				}
+
+				return [newTodoData, ...prevTodos]
+			})
 			setNewTodo('')
 		}
 	}
+
+	const removeScroll = () => {
+		ulRef.current.removeEventListener('scroll', handleScroll)
+	}
+
+	useEffect(() => {
+		ulRef.current.addEventListener('scroll', handleScroll)
+	}, [])
 
 	return (
 		<>
 			<div className='wrapper'>
 				<h1>Todo list</h1>
-				<ul className='todos'>
+				<ul className='todos' ref={ulRef}>
 					{todos.map(todo => (
 						<li key={todo.id} className='todo-item'>
 							{todo.title}
@@ -26,6 +47,7 @@ function App() {
 					))}
 				</ul>
 				<div className='todo-footer'>
+					<button onClick={removeScroll}>Remove scroll</button>
 					<div className='todo-input'>
 						<input
 							type='text'
