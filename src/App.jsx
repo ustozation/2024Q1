330ca40f3ee5c113522f275279e4844d65fe757c
Ali Odiljonov <jahcoding@gmail.com>
2024-02-11 21:01:46 +0500

    feat: add setCount in inc function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(5)
 	const inc = () => {
 		setCount(count + 1)
+		// setCount(prevState => {
+		// 	return prevState + 1
+		// })
 	}
 
 	return (
