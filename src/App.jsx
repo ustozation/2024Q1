afe94dd86260272d48545f7a7a6d069bfb05cc16
Ali Odiljonov <jahcoding@gmail.com>
2024-02-11 20:26:33 +0500

    feat: add useRef lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { todosList } from './utils/mock'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(5)
-	const inc = () => {
-		setCount(count + 1)
-	}
+	const [todos, setTodos] = useState(todosList)
+	const [newTodo, setNewTodo] = useState('')
 
-	const dec = () => {
-		if (count > 0) {
-			setCount(count - 1)
+	const addTodo = () => {
+		if (newTodo) {
+			setTodos([{ id: uuidv4(), title: newTodo, completed: false }, ...todos])
+			setNewTodo('')
 		}
 	}
 
 	return (
 		<>
-			<div className='card'>
-				<h1>{count}</h1>
-				<button onClick={inc}>INC</button>
-				{count > 0 && <button onClick={dec}>DEC</button>}
+			<div className='wrapper'>
+				<h1>Todo list</h1>
+				<ul className='todos'>
+					{todos.map(todo => (
+						<li key={todo.id} className='todo-item'>
+							{todo.title}
+						</li>
+					))}
+				</ul>
+				<div className='todo-footer'>
+					<div className='todo-input'>
+						<input
+							type='text'
+							onChange={e => setNewTodo(e.target.value)}
+							value={newTodo}
+						/>
+						<button onClick={addTodo}>ADD</button>
+					</div>
+				</div>
 			</div>
 		</>
 	)
---
+++ src/utils/mock/index.js
@@ -0,0 +1,52 @@
+export const todosList = [
+	{
+		id: 1,
+		title: 'Schedule dentist appointment.',
+		completed: false,
+	},
+	{
+		id: 2,
+		title: 'Call Sarah regarding the project updates',
+		completed: true,
+	},
+	{
+		id: 3,
+		title: 'Pay the utility bills',
+		completed: false,
+	},
+	{
+		id: 4,
+		title: 'Exercise for 30 minutes',
+		completed: false,
+	},
+	{
+		id: 5,
+		title: 'Read one chapter of the book before bed',
+		completed: true,
+	},
+	{
+		id: 6,
+		title: 'Water the plants',
+		completed: false,
+	},
+	{
+		id: 7,
+		title: 'Organize files and documents on the computer',
+		completed: false,
+	},
+	{
+		id: 8,
+		title: 'Meal prep for the rest of the week',
+		completed: true,
+	},
+	{
+		id: 9,
+		title: 'Meditate for 15 minutes',
+		completed: false,
+	},
+	{
+		id: 10,
+		title: 'Prepare for the marketing campaign',
+		completed: false,
+	},
+]
