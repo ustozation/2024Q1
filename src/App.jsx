55f64d2a317055f3f41c93b82c4169e695bbbfac
Ali Odiljonov <jahcoding@gmail.com>
2024-02-11 22:04:09 +0500

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(5)
-	const inc = () => {
-		setCount(count + 1)
-	}
-
-	const dec = () => {
-		if (count > 0) {
-			setCount(count - 1)
-		}
-	}
-
 	return (
 		<>
 			<div className='card'>
-				<h1>{count}</h1>
-				<button onClick={inc}>INC</button>
-				{count > 0 && <button onClick={dec}>DEC</button>}
+				<h1>2024Q1</h1>
 			</div>
 		</>
 	)
