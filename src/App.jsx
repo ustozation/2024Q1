b3148ed6582f18ea1ee0016d20426635b7a7f9b9
Ali Odiljonov <jahcoding@gmail.com>
2024-02-11 18:58:38 +0500

    feat: add useState lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -7,18 +7,11 @@ function App() {
 		setCount(count + 1)
 	}
 
-	const dec = () => {
-		if (count > 0) {
-			setCount(count - 1)
-		}
-	}
-
 	return (
 		<>
 			<div className='card'>
-				<h1>{count}</h1>
-				<button onClick={inc}>INC</button>
-				{count > 0 && <button onClick={dec}>DEC</button>}
+				<h1></h1>
+				<button onClick={inc}>count is {count}</button>
 			</div>
 		</>
 	)
